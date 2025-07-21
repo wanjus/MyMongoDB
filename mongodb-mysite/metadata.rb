a080b4bb3d63429daf7b93f8f3caeb4edf241d55
wanjus <wanjus@users.noreply.github.com>
2025-07-21 06:45:16 +0200

    Delete mongodb-mysite directory

--- mongodb-mysite/metadata.rb
+++
@@ -1,7 +0,0 @@
-name        "mongodb-mysite"
-description 'Example cookbook for MongoDB Berkshelf based install'
-maintainer  "MyOrg"
-license     "Apache 2.0"
-version     "1.0.0"
-
-depends 'mongodb'
